d7defa6ecc760cda513daef6cffe18102068cb5d
jub3907 <side1923@naver.com>
2022-01-20 17:02:26 +0900

    환경파일 추가 및 Configuration 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (config: ConfigService) => {
+        return {
+          uri: config.get('db'),
+        };
+      },
+      inject: [ConfigService],
+    }),
+
+    // GraphQLModule.forRoot({
+    //   path: '/',
+    //   autoSchemaFile: 'schema.gql',
+    //   playground: process.env.NODE_ENV === 'development',
+    //   debug: process.env.NODE_ENV === 'development',
+
+    //   context: ({ req, res }) => ({ req, res }),
+    //   sortSchema: true,
+    // }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
---
+++ src/config/configuration.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  port: process.env.PORT || 3333,
+  db: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
+});
