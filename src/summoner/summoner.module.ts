2387234cc6ee3f8dd1a8375f9e6acc9bae22d19f
jub3907 <side1923@naver.com>
2022-01-22 18:46:26 +0900

    mastery 모듈 삭제

--- src/summoner/summoner.module.ts
+++ src/summoner/summoner.module.ts
@@ -3,16 +3,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SummonerResolver } from './summoner.resolver';
 import { Summoner, SummonerSchema } from './schema/summoner.schema';
 import { SummonerService } from './summoner.service';
-import { MasteryModule } from './mastery.module';
+import { MasteryResolver } from './mastery.resolver';
+import { MasteryService } from './mastery.service';
 
 @Module({
   imports: [
     MongooseModule.forFeatureAsync([
       { name: Summoner.name, useFactory: () => SummonerSchema },
     ]),
-    MasteryModule,
+    // MasteryModule,
   ],
-  providers: [SummonerResolver, SummonerService],
+  providers: [
+    SummonerResolver,
+    SummonerService,
+    MasteryService,
+    MasteryResolver,
+  ],
   exports: [MongooseModule, SummonerService],
 })
 export class SummonerModule {}
