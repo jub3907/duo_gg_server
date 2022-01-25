162210338b79cb10a7179ec645ae3c9b733c9171
jub3907 <side1923@naver.com>
2022-01-25 10:22:20 +0900

    match의 base resolver에 타입 상속 추가

--- src/match/match-base.resolver.ts
+++ src/match/match-base.resolver.ts
@@ -1,7 +1,8 @@
+import { Type } from '@nestjs/common';
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { MatchDocument } from './schema/match.schema';
 
-export function MatchBaseResolver<T>(model: T): any {
+export function MatchBaseResolver<T extends Type<unknown>>(model: T): any {
   @Resolver((of) => model, { isAbstract: true })
   abstract class MatchBase {
     @ResolveField((returns) => String)
