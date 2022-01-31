39ae5deb000dcfd30a5faf2a3f846919d81b4f21
jub3907 <side1923@naver.com>
2022-01-31 13:11:07 +0900

    match-basic에서 불필요한 ResolveField 제거

--- src/match/match-basic.resolver.ts
+++ src/match/match-basic.resolver.ts
@@ -31,22 +31,6 @@ export class MatchBasicResolver extends MatchBaseResolver(MatchBasicModel) {
   ) {
     super();
   }
-
-  @ResolveField((returns) => String)
-  matchType(@Parent() match: MatchDocument) {
-    return match.queueId === 420
-      ? '솔로 랭크'
-      : match.queueId === 430
-      ? '일반 게임'
-      : match.queueId === 440
-      ? '자유 5:5 랭크'
-      : match.queueId === 450
-      ? '무작위 총력전'
-      : match.queueId === 1400
-      ? '궁극기 주문서'
-      : '기타';
-  }
-
   @ResolveField((returns) => ParticipantDto)
   summonerInGameData(@Parent() match: MatchBasicModel) {
     return match.participants.find(({ puuid }) => puuid === match.puuid);
